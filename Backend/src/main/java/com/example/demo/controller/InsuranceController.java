abfd7b7d25732a4d035717c41a2a94fccfc4a6f5
Vaishnavi <vaishnavimarkad64@gmail.com>
2026-06-25 19:13:01 +0530

    Remove Space

--- Backend/src/main/java/com/example/demo/controller/InsuranceController.java
+++ Backend/src/main/java/com/example/demo/controller/InsuranceController.java
@@ -5,34 +5,23 @@ import com.example.demo.model.Insurance;
 import com.example.demo.model.LoginRequest;
 import com.example.demo.service.InsuranceService;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.HashMap;
 import java.util.Map;
-
-
 
 @RestController
 @CrossOrigin(origins = "*")
 public class InsuranceController {
 
-
-
     @Autowired
     private InsuranceService sr;
-
-
-
-
 
     @PostMapping("/insurances")
     public Insurance saveInsurance(
             @RequestBody Insurance insurance){
-
 
         return sr.saveInsurance(insurance);
 
@@ -42,59 +31,37 @@ public class InsuranceController {
     public ResponseEntity<?> login(
             @RequestBody LoginRequest request){
 
-
-
         Insurance user =
                 sr.login(request);
 
-
-
-
         if(user == null){
-
 
             Map<String,String> error =
                     new HashMap<>();
-
 
             error.put(
                     "message",
                     "Invalid email/username/mobile or password"
             );
 
-
             return ResponseEntity
                     .badRequest()
                     .body(error);
-
         }
-
-
-
-
-
 
         Map<String,Object> response =
                 new HashMap<>();
-
 
         response.put(
                 "token",
                 "dummy-token"
         );
 
-
         response.put(
                 "user",
                 user
         );
 
-
-
         return ResponseEntity.ok(response);
-
     }
-
-
-
 }
--- Backend/src/main/java/com/example/demo/model/LoginRequest.java
+++ Backend/src/main/java/com/example/demo/model/LoginRequest.java
@@ -5,25 +5,20 @@ public class LoginRequest {
     private String identifier;
     private String password;
 
-
     public LoginRequest() {
     }
-
 
     public String getIdentifier() {
         return identifier;
     }
 
-
     public void setIdentifier(String identifier) {
         this.identifier = identifier;
     }
 
-
     public String getPassword() {
         return password;
     }
-
 
     public void setPassword(String password) {
         this.password = password;
